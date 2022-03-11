e6aa26e17c80bf6da3d1bf329dc00cf022ea8414
Younes <dev@younes.io>
2022-03-11 17:29:22 +0100

    WIP: testing race conditions

--- __tests__/reservations_concurrency.test.ts
+++ __tests__/reservations_concurrency.test.ts
@@ -1,81 +1,68 @@
-import fc from 'fast-check';
 import supertest from 'supertest';
 import app from '../src/app';
-// import { createMockContext } from '@shopify/jest-koa-mocks';
-import {
-    bookReservation,
-    getAvailableReservation,
-} from '../src/middlewares/reservations/helpers';
-// import { prismaMock } from './mocks/prisma.mock';
+import prisma from '../src/database';
 
 const request = supertest(app.callback());
 
-jest.mock('../src/middlewares/reservations/helpers');
-const mockedBookReservation = jest.fn() as jest.MockedFunction<
-    typeof bookReservation
->;
-const mockedGetAvailableReservation = jest.fn() as jest.MockedFunction<
-    typeof getAvailableReservation
->;
+describe.skip('WIP: Booking reservations : race conditions', () => {
+    beforeAll(async () => {
+        // Add tables to the restaurant
+        await prisma.reservation.createMany({
+            data: [
+                {
+                    slotId: '622b760b41fe0ccd54121cc5',
 
-describe('Booking reservations : race conditions', () => {
+                    tableId: '622b75fd41fe0ccd54121cc3',
+                    tableName: 'MilanoTest',
+                    slotStartHour: '19:00',
+                    businessDay: '17-03-2022',
+                    booked: false,
+                },
+                {
+                    slotId: '622b760b41fe0ccd54121cc6',
+                    tableId: '622b75fd41fe0ccd54121cc3',
+                    tableName: 'MilanoTest',
+                    slotStartHour: '20:00',
+                    businessDay: '17-03-2022',
+                    booked: false,
+                },
+                {
+                    slotId: '622b760b41fe0ccd54121cc7',
+                    tableId: '622b75fd41fe0ccd54121cc3',
+                    tableName: 'MilanoTest',
+                    slotStartHour: '21:00',
+                    businessDay: '17-03-2022',
+                    booked: false,
+                },
+                {
+                    slotId: '622b760b41fe0ccd54121cc8',
+                    tableId: '622b75fd41fe0ccd54121cc3',
+                    tableName: 'MilanoTest',
+                    slotStartHour: '22:00',
+                    businessDay: '17-03-2022',
+                    booked: false,
+                },
+            ],
+        });
+    });
+
     it('should NOT book the same reservation (table+timeslot) for two customers', async () => {
-        fc.assert(
-            fc
-                .asyncProperty(
-                    fc.scheduler(),
-                    fc.uuid(),
-                    async (s, reservationId) => {
-                        // Arrange
-                        // prismaMock.reservation.findFirst.mockResolvedValue(
-                        //     reservation,
-                        // );
-                        const reservation = {
-                            id: reservationId,
-                            slotId: 'randomId',
-                            slotStartHour: '20:00',
-                            tableId: 'randomId',
-                            tableName: 'Milano',
-                            businessDay: '16-03-2022',
-                            booked: false,
-                        };
-                        mockedGetAvailableReservation.mockResolvedValue(
-                            reservation,
-                        );
-                        // prismaMock.reservation.update.mockResolvedValue({
-                        //     ...reservation,
-                        //     booked: true,
-                        // });
-                        mockedBookReservation.mockImplementation(
-                            s.scheduleFunction(async (reservationId) => {
-                                return { ...reservation, id: reservationId };
-                            }),
-                        );
+        const res = await request
+            .post('/api/v1/reservation')
+            .set('Content-Type', '"application/json; charset=utf-8"')
+            .set('Accept', 'application/json')
+            .send({
+                timeSlot: '20:00',
+                tableName: 'MilanoTest',
+                businessDay: '17-03-2022',
+            });
 
-                        const res = await request
-                            .post('/api/v1/reservation')
-                            .set(
-                                'Content-Type',
-                                '"application/json; charset=utf-8"',
-                            )
-                            .set('Accept', 'application/json')
-                            .send({
-                                timeSlot: '20:00',
-                                tableName: 'Milano',
-                                businessDay: '16-03-2022',
-                            });
-                        await s.waitOne();
-                        expect(res).toBe(true);
+        expect(res).toBe(true);
+        expect(1 + 1).toBe(2);
+    });
 
-                        expect(1 + 1).toBe(2);
-                        // expect(res).toBe(2);
-                        // expect(1 + 1).toBe(2);
-                    },
-                )
-                .beforeEach(async () => {
-                    jest.resetAllMocks();
-                }),
-            { verbose: 2 },
-        );
+    afterAll(async () => {
+        await prisma.reservation.deleteMany();
+        await prisma.$disconnect();
     });
 });
